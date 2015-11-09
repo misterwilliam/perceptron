f7770dd7d497445e6a71e176df066b50b82db137
William Chang <mr.williamchang@gmail.com>
2015-11-08 23:04:36 -0800

    Still some bugs

--- main.go
+++ main.go
@@ -35,60 +35,54 @@ func VectorSubtract(a []float64, b []float64) []float64 {
   return retVal
 }
 
-func Score(featureVector []float64, model []float64, threshold float64) float64 {
-  return Dot(featureVector, model) - threshold
+func Score(featureVector []float64, model []float64) float64 {
+  return Dot(featureVector, model)
 }
 
-func Classify(featureVector []float64, model []float64, threshold float64) bool {
-  return Score(featureVector, model, threshold) > 0
+func Classify(featureVector []float64, model []float64,) bool {
+  return Score(featureVector, model) > 0
 }
 
-func Train(trainingData []LabeledVector) ([]float64, float64) {
+func Train(trainingData []LabeledVector) []float64 {
   model := make([]float64, len(trainingData[0].featureVector))
-  threshold := 0.0
-  for i := 0; i < 100; i++ {
+  for i := 0; i < 10000; i++ {
     numRight := 0
     numWrong := 0
     for _, trainingSample := range trainingData {
-      modelLabel := Classify(trainingSample.featureVector, model, threshold)
+      modelLabel := Classify(trainingSample.featureVector, model)
       if modelLabel == trainingSample.label {
         numRight++
         continue
       } else if trainingSample.label {
         numWrong++
-        // trainingSample label is true, lower threshold and add training sample feature
-        // vector to model
-        threshold--
+        // trainingSample label is true, add training sample feature vector to model
         model = VectorAdd(model, trainingSample.featureVector)
       } else if !trainingSample.label {
         numWrong++
-        // trainingSample label is false, increase threshold and subtract training sample
-        // feature vector to model
-        threshold++
+        // trainingSample label is false, subtract training sample feature vector to model
         model = VectorSubtract(model, trainingSample.featureVector)
       }
     }
-    model, threshold = Normalize(model, threshold)
-    fmt.Println(model, threshold, float32(numRight) / float32(numWrong + numRight))
+    model = NormalizeVector(model)
   }
-  return model, threshold
+  return model
 }
 
 func GenTrainingSamples(model []float64,
-                        threshold float64,
                         numSamples int) []LabeledVector {
   // make feature vector
-  stdDev := 10.5
+  stdDev := 0.0
   vectorSize := len(model)
   samples := make([]LabeledVector, numSamples)
   for i := 0; i < numSamples; i++ {
-    // Make random weights
+    // Make random normalized weights
     featureVector := make([]float64, vectorSize)
     for j := 0; j < vectorSize; j++ {
       featureVector[j] = rand.Float64()
     }
+    featureVector = NormalizeVector(featureVector)
     // Assign a label according to model
-    score := Score(featureVector, model, threshold)
+    score := Score(featureVector, model)
     samples[i] = LabeledVector{
                    featureVector,
                    rand.NormFloat64() * stdDev + score > 0.0,
@@ -97,20 +91,20 @@ func GenTrainingSamples(model []float64,
   return samples
 }
 
-func Normalize(model []float64, threshold float64) ([]float64, float64) {
-  if threshold == 0.0 {
-    return model, threshold
+// Normalize vector so that Euclidean distance is 1
+func NormalizeVector(vector []float64) []float64 {
+  euclideanDistance := math.Sqrt(Dot(vector, vector))
+  retVal := make([]float64, len(vector))
+  for i, val := range vector {
+    retVal[i] = val / euclideanDistance
   }
-  absoluteThreshold := math.Abs(threshold)
-  normalizedModel := make([]float64, len(model))
-  for i := range(model) {
-    normalizedModel[i] = model[i] / absoluteThreshold
-  }
-  return normalizedModel, threshold / absoluteThreshold
+  return retVal
 }
 
 func main() {
-  trainingData := GenTrainingSamples([]float64{0.3, -0.1, 0.1, -0.1, 0.4}, 1.0, 100)
-  model, threshold := Train(trainingData)
-  fmt.Println(Normalize(model, threshold))
+  trueModel := NormalizeVector([]float64{0.3, -0.1, 0.1, -0.1, 0.4})
+  trainingData := GenTrainingSamples(trueModel, 50000)
+  model := Train(trainingData)
+  fmt.Println(model)
+  fmt.Println("Actual model", trueModel)
 }
