c84aed1a5538cbeb97fd488ab84a212bde159a51
William Chang <mr.williamchang@gmail.com>
2015-11-08 21:53:53 -0800

    Some bug fixes

--- main.go
+++ main.go
@@ -46,21 +46,30 @@ func Classify(featureVector []float64, model []float64, threshold float64) bool 
 func Train(trainingData []LabeledVector) ([]float64, float64) {
   model := make([]float64, len(trainingData[0].featureVector))
   threshold := 0.0
-  for i := 0; i < 200; i++ {
+  for i := 0; i < 100; i++ {
+    numRight := 0
+    numWrong := 0
     for _, trainingSample := range trainingData {
       modelLabel := Classify(trainingSample.featureVector, model, threshold)
       if modelLabel == trainingSample.label {
+        numRight++
         continue
-      } else if modelLabel {
-        // modelLabel is true trainingSample label is false
+      } else if trainingSample.label {
+        numWrong++
+        // trainingSample label is true, lower threshold and add training sample feature
+        // vector to model
         threshold--
         model = VectorAdd(model, trainingSample.featureVector)
-      } else if !modelLabel {
-        // modelLabel if false and trainingSample label is true
+      } else if !trainingSample.label {
+        numWrong++
+        // trainingSample label is false, increase threshold and subtract training sample
+        // feature vector to model
         threshold++
         model = VectorSubtract(model, trainingSample.featureVector)
       }
     }
+    model, threshold = Normalize(model, threshold)
+    fmt.Println(model, threshold, float32(numRight) / float32(numWrong + numRight))
   }
   return model, threshold
 }
@@ -69,14 +78,16 @@ func GenTrainingSamples(model []float64,
                         threshold float64,
                         numSamples int) []LabeledVector {
   // make feature vector
-  stdDev := 1.0
+  stdDev := 10.5
   vectorSize := len(model)
   samples := make([]LabeledVector, numSamples)
   for i := 0; i < numSamples; i++ {
+    // Make random weights
     featureVector := make([]float64, vectorSize)
     for j := 0; j < vectorSize; j++ {
-      featureVector[j] = rand.NormFloat64() * stdDev
+      featureVector[j] = rand.Float64()
     }
+    // Assign a label according to model
     score := Score(featureVector, model, threshold)
     samples[i] = LabeledVector{
                    featureVector,
@@ -87,20 +98,19 @@ func GenTrainingSamples(model []float64,
 }
 
 func Normalize(model []float64, threshold float64) ([]float64, float64) {
-  absoluteValue := math.Abs(threshold)
-  for _, val := range model {
-    absoluteValue += math.Abs(val)
+  if threshold == 0.0 {
+    return model, threshold
   }
+  absoluteThreshold := math.Abs(threshold)
   normalizedModel := make([]float64, len(model))
   for i := range(model) {
-    normalizedModel[i] = model[i] / absoluteValue
+    normalizedModel[i] = model[i] / absoluteThreshold
   }
-  return normalizedModel, threshold / absoluteValue
+  return normalizedModel, threshold / absoluteThreshold
 }
 
 func main() {
-  trainingData := GenTrainingSamples([]float64{0.2, 0.3, -0.2}, 0.3, 2000)
-
+  trainingData := GenTrainingSamples([]float64{0.3, -0.1, 0.1, -0.1, 0.4}, 1.0, 100)
   model, threshold := Train(trainingData)
   fmt.Println(Normalize(model, threshold))
 }
