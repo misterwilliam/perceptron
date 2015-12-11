60d62644c6fb96bec31bf41b0c4ec79bc0fd1095
William Chang <mr.williamchang@gmail.com>
2015-12-11 15:40:43 -0800

    Rename to perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -5,13 +5,14 @@ from vector_utils import dot, normalize, sign
 
 class Perceptron(object):
 
-  def __init__(self, dimensions):
+  def __init__(self, dimensions, onIteration=None):
     self.dimensions = dimensions
     self.weights = [0.0 for _ in xrange(dimensions)]
     self.bias = 0.0
+    self.onIteration = onIteration
 
   def train(self, data, num_iters):
-    for _ in xrange(num_iters):
+    for iteration_count in xrange(num_iters):
       num_errors = 0
       for vector, label in data:
         activation = dot(self.weights, vector) + self.bias
@@ -21,7 +22,8 @@ class Perceptron(object):
             self.weights[i] += label * vector[i]
           self.bias += label
           num_errors += 1
-      print num_errors
+        if self.onIteration is not None:
+          self.onIteration(iteration_count, self.weights, self.bias, num_errors, len(data))
     return self.weights, self.bias
 
 def gen_random_training_data(num_examples, dimensions):
@@ -34,21 +36,6 @@ def gen_random_training_data(num_examples, dimensions):
     training_data.append((feature_vector, classification))
   return training_data, weights, bias
 
-def train(data, dimensions, max_iter):
-  weights = [0.0 for _ in xrange(dimensions)]
-  bias = 0.0
-  for _ in xrange(max_iter):
-    num_errors = 0
-    for vector, label in data:
-      activation = dot(weights, vector) + bias
-      if label * activation <= 0:
-        # Update weights
-        for i in xrange(dimensions):
-          weights[i] += label * vector[i]
-        bias += label
-        num_errors += 1
-  return weights, bias
-
 def classify(vector, weights, bias):
   activation = dot(vector, weights) + bias
   return sign(activation)
@@ -58,14 +45,18 @@ def classify(vector, weights, bias):
 class BenchmarkTests(unittest.TestCase):
 
   def test_BenchmarkAccuracy(self):
-    training_data, true_weights, true_bias = gen_random_training_data(10000, 15)
-    p = Perceptron(15)
-    p.train(training_data, 30)
-    #trained_weights, trained_bias = train(training_data, 15, 30)
-    #print normalize(true_weights, true_bias), normalize(trained_weights, trained_bias)
+    good_enough_iteration = []
+    def handle_iteration(iteration_count, weights, bias, num_errors, num_training_examples):
+        if num_errors / float(num_training_examples) < 0.01:
+            good_enough_iteration.append(iteration_count)
+    for _ in xrange(10):
+        training_data, true_weights, true_bias = gen_random_training_data(10000, 15)
+        p = Perceptron(15, onIteration=handle_iteration)
+        p.train(training_data, 30)
+    print "Average iterations till good enough: %f" % (
+        sum(good_enough_iteration) / len(good_enough_iteration))
 
-
-class TrainTests(unittest.TestCase):
+class PerceptronTests(unittest.TestCase):
 
   def test_SmallSingleStepExample(self):
     data = [
